237a330e6bcd6608faeb1e7f5a5e0afa8d4aca78
Kevin Chen <kchen@lcs.on.ca>
2025-02-24 17:52:54 -0500

    completed ItemView and some View

--- TimeConverter/Views/HourConversionItemView.swift
+++ TimeConverter/Views/HourConversionItemView.swift
@@ -7,3 +7,17 @@
 
 import SwiftUI
 
+struct HourConversionItemView: View {
+    //Stored properties
+    let conversionResult: HourConversion
+    
+    //Computed properties
+    var body: some View {
+        HStack(spacing: 0) {
+            Text("\(conversionResult.timeInHours.formatted()) minutes, ")
+            
+            Text("\(conversionResult.timeInMinutes.formatted()) seconds")
+        }
+        .font(.largeTitle)
+    }
+}
--- TimeConverter/Views/HourConversionView.swift
+++ TimeConverter/Views/HourConversionView.swift
@@ -7,3 +7,29 @@
 
 import SwiftUI
 
+struct HourConversionView: View {
+    
+    //Stored properties
+    @State var viewModel = HourConversionViewModel()
+    
+    //Computed properties
+    var body: some View {
+        VStack {
+            //Output
+            if let conversionResult = viewModel.conversionResult {
+                
+                VStack(spacing: 0) {
+                    
+                    HourConversionItemView(conversionResult: conversionResult)
+                        .padding(.bottom, 30)
+                    
+                    
+                    Button
+                    
+                }
+                
+                
+            }
+        }
+    }
+}
